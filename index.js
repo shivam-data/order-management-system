16f36099669374ba974cc309ae63b9165de56891
Shivam Patel <shivam@glib.ai>
2020-09-21 13:16:43 +0530

    done

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ const mongoose = require('mongoose')
 var nunjucks  = require('nunjucks');
 const session = require('express-session')
 
+require('dotenv').config()
 
-mongoose.connect(process.env.MONGODB_URI,{useNewUrlParser:true,useUnifiedTopology:true});
+
+mongoose.connect(process.env.MONGO_URI,{useNewUrlParser:true,useUnifiedTopology:true});
 
 const app = express()
 
@@ -15,8 +17,6 @@ nunjucks.configure(['views/'],{
   autoescape:false,
   express:app
 })
-
-require('dotenv').config()
 
 app.use(session({
     secret: "supersecretfile",
@@ -36,4 +36,4 @@ app.use(express.urlencoded({extended:false}))
 app.use('/',AuthRoute)
 app.use('/',BusinessRoute)
 
-app.listen(process.env.PORT )
+app.listen(6969)
